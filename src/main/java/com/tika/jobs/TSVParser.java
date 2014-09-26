3e5de5b04a93a2a57ec6f847e969959958ab633b
Poojit Sharma <poojits@gmail.com>
2014-09-25 23:37:46 -0700

    Add initial implementation of TSVParser

--- src/main/java/com/tika/jobs/TSVParser.java
+++ src/main/java/com/tika/jobs/TSVParser.java
@@ -1,4 +1,4 @@
-package com.tika.jobs;
+package org.apache.tika.parser.tsv;
 
 import org.apache.tika.config.ServiceLoader;
 import org.apache.tika.detect.AutoDetectReader;
@@ -14,8 +14,12 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
+
 
 public class TSVParser extends AbstractParser {
 
@@ -28,22 +32,44 @@ public class TSVParser extends AbstractParser {
 
     private static final ServiceLoader LOADER = new ServiceLoader(TSVParser.class.getClassLoader());
 
-    public void parse(InputStream stream, ContentHandler handler,Metadata metadata, ParseContext context) throws IOException, SAXException, TikaException {
-        AutoDetectReader reader = new AutoDetectReader(new CloseShieldInputStream(stream), metadata, context.get(ServiceLoader.class, LOADER));
+    public void parse(
+            InputStream stream, ContentHandler handler,
+            Metadata metadata, ParseContext context)
+            throws IOException, SAXException, TikaException {
+
+        // Automatically detect the character encoding
+        AutoDetectReader reader = new AutoDetectReader(
+                new CloseShieldInputStream(stream), metadata,
+                context.get(ServiceLoader.class, LOADER));
         try {
+            MediaType type = new MediaType(MediaType.TEXT_HTML, Charset.forName("UTF-8"));
+            metadata.set(Metadata.CONTENT_TYPE, type.toString());
+            metadata.set(Metadata.CONTENT_ENCODING, Charset.forName("UTF-8").name());
+            ArrayList<String> headings = new ArrayList(Arrays.asList("postedDate","location","department","title","salary","start","duration","jobtype","applications","company","contactPerson","phoneNumber","faxNumber","location","latitude","longitude","firstSeenDate","url","lastSeenDate"));
             XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
             xhtml.startDocument();
-            xhtml.startElement("p");
-            char[] buffer = new char[4096];
-            int n = reader.read(buffer);
-            while (n != -1) {
-                xhtml.characters(buffer, 0, n);
-                n = reader.read(buffer);
+            xhtml.startElement("table");
+            xhtml.startElement("tr");
+            for(int i=0;i<headings.size();i++){
+                xhtml.startElement("th");
+                xhtml.characters(headings.get(i));
+                xhtml.endElement("th");
             }
-            xhtml.endElement("p");
+            xhtml.endElement("tr");
+            while(reader.ready()){
+                xhtml.startElement("tr");
+                String line = reader.readLine();
+                String[] cols = line.split("\t");
+                for(int i=0;i<cols.length;i++){
+                    xhtml.startElement("td");
+                    xhtml.characters(cols[i]);
+                    xhtml.endElement("td");
+                }
+                xhtml.endElement("tr");
+            }
+            xhtml.endElement("table");
             xhtml.endDocument();
-        }
-        finally {
+        } finally {
             reader.close();
         }
     }
